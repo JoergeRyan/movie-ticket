11a05786a2d5f4c2bf735fbceb099fab15e5158e
Joerge Ryan Lou <joergeryanlou.aws@gmail.com>
2023-11-30 20:19:06 +0800

    added seat

--- server/models/movieModel.js
+++ server/models/movieModel.js
@@ -8,31 +8,28 @@ const movieSchema = new mongoose.Schema(
     },
     description: {
       type: String,
-      required: true,
     },
     duration: {
       type: Number,
-      required: true,
     },
     genre: {
       type: String,
-      required: true,
     },
     language: {
       type: String,
-      required: true,
     },
     releaseDate: {
       type: Date,
-      required: true,
     },
     poster: {
       type: String,
-      required: true,
     },
   },
   { timestamps: true }
 );
 
 
+
 module.exports = mongoose.model("movies", movieSchema);
+
+
